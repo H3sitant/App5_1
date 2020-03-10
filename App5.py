005f901d6de3eec9769b3ace600e8dba34d6d638
isack <isackbreton@live.ca>
2020-03-10 13:08:50 -0400

    graph

--- App5.py
+++ App5.py
@@ -50,29 +50,95 @@ import argparse
 import glob
 import sys
 import os
+import networkx as nx
 from pathlib import Path
 from random import randint
 from random import choice
 
+
+import time
 ### Ajouter ici les signes de ponctuation Ã  retirer
 PONC = ["!", '"', "'", ")", "(", ",", ".", ";", ":", "?", "-", "_", "\'", "\"", "\\", "»", "«", "\n"]
 
 ###  Vous devriez inclure vos classes et méthodes ici, qui seront appellées Ã  partir du main
+def Generation(graph1):
+    i = 1
+    NText = ''
+    di2 = dict()
+    NText = list(graph1)[randint(0, len(list(graph1))-args.m)]
+    i = args.m - 1
+    while i < args.G:
+        if args.m == 1:
+            NText = NText + ' ' + k[randint(0, len(k)-1)]
+        else:
+            NText = NTextGramme(NText, k)
+        i += 1
+    print(NText)
+    mots = NText.split()
+    di2 = gramme(mots, di2)
+    di2 = valeurPourCent(di, len(mots))
+    return di2
+def NTextGramme(NText,k):
+    A = NText.split()
+    B = A[len(A)-args.m]
+    i = args.m-1
+    while i > 0:
+        B = B + ' ' + A[len(A)-i]
+        i -= 1
+    print(B)
+    time.sleep(2.4)
+    D = ''
+    while D != B:
+        E = randint(0, len(k) - args.m)
+        C = k[E].split()
+        D = C[0]
+        i = 1
+        while i < args.m:
+            print(i)
+            print(C)
+            D = D + ' ' + C[i]
+            i += 1
+        print(D)
+    i=1
+    print(C)
+    while i<len(C):
+        NText = NText + ' ' + C[i]
+        i+=1
+    return NText
 def comparaison(text, librairie):
     ressemblence=0
     for k, v in text.items():
         if k in librairie:
             ressemblence += ((v ** 2) + (librairie[k] ** 2)) ** 0.5
+    if ressemblence > 1:
+        ressemblence = 1
     return ressemblence
+def grammeG(mots,GMots):
+    i = 0
+    for w in mots:
+        if len(w) >= 3:
+            w = str.lower(w)
+            g = 1
+            if w not in GMots:
+                GMots.add_node(w)
+            while i + g < len(mots) and len(mots[i + g]) < 3:
+                g = g + 1
+            if g + i < len(mots):
+                mots[i + g] = str.lower(mots[i + g])
+                if mots[i+g] not in GMots:
+                    GMots.add_node(mots[i+g])
+                GMots.add_edge(w, mots[i+g])
+            g = g + 1
+        i = i + 1
+    return GMots
 def gramme(mots,di):
-    i=0
-    mots2=mots
+    i = 0
+    mots2 = mots
     for w in mots:
         if len(w) >= 3:
             k = 1
             g = k
             mots2[i] = w
-
             while k < args.m:
 
                 while i+g < len(mots) and len(mots[i+g]) < 3:
@@ -99,20 +165,25 @@ def Ponctuation(ligne):
         if char not in PONC:
             no_punct = no_punct + char
     return no_punct
-def lecture(di):
+def lecture(di, G):
     longeur = 0
     for file in glob.glob('*.txt'):
         print(file + '\n')
         f = open(file, 'r', encoding='utf-8')
-        line2=''
+        line2 = ''
         for line in f:
-            line2=line2+' '+line
-        mots = Ponctuation(line2).split()
+            line2 = line2 + ' ' + Ponctuation(line)
+        mots = line2.split()
+
         di = gramme(mots, di)
+        G = grammeG(list(di),G)
+        print(G.nodes())
+        print(G.edges())
+
         longeur = longeur + len(mots)
         f.close()
     di= valeurPourCent(di, longeur)
-    return di
+    return di, G
 
 
 ### Main: lecture des paramÃ¨tres et appel des mÃ©thodes appropriÃ©es
@@ -185,22 +256,29 @@ if __name__ == "__main__":
 
 ### Ã€ partir d'ici, vous devriez inclure les appels Ã  votre code
     j = 1
-    librairie = []
+    librairieDI = []
+    librairieG = []
+    G = nx.Graph()
+    ##Lecture et enregistrement
     for a in authors:
         aut = a.split("/")
         di = dict()
         if aut[-1] != '.DS_Store':
-            os.chdir(cwd+'\\'+args.d + '\\'+ aut[-1])
+            os.chdir(cwd+'\\'+args.d + '\\' + 'Isack')
             di = dict()
-            di = lecture(di)
+            (di, G) = lecture(di, G)
             ##for k, v in di.items():
                 ##print(k,v)
 
-        librairie.append(di)
+        librairieDI.append(di)
+        librairieG.append(G)
         j += 1
     i = 1
+    ## génération
+    ##librairieDI.append(Generation(choice(librairieG)))
+    ##Comparaison
     for a in authors:
         if a != '.DS_Store':
-            R = comparaison(librairie[1], librairie[i])## remplacer ***librairi[1]*** par le dict() du nouveau text
+            R = comparaison(librairieDI[len(authors)-1], librairieDI[i]) ## remplacer ***librairi[1]*** par le dict() du nouveau text
             print(a+" : %0.2f" % R)
             i += 1
